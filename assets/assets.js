5f37d3ab2f28b9a01ec6fc04a3ec1872ccf59d25
aapka-naya-github-username <rabiasehar0820@gmail.com>
2025-04-07 04:00:30 +0500

    MERN Portfolio Complete 7/2025!

---
+++ assets/assets.js
@@ -0,0 +1,139 @@
+import user_image from './user-image.png';
+import my_img from './my_img.jpeg';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.jpeg';
+import logo_dark from './logo_dark.jpeg';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import react_icon from './react_icon.png';
+import mui_icon from './mui_icon.png';
+import tailwind_icon from './tailwind_icon.png';
+import bot_icon from './bot_icon.png';
+import front_dev_icon from './front_dev_icon.jpg';
+import backend_icon from './backend_icon.png';
+import single_page_icon from './single_page_icon.png';
+import weeb_icon from './web_icon.png';
+
+export const assets = {
+    user_image,
+    my_img,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    firebase,
+    figma,
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark,
+    react_icon,
+    mui_icon,
+    tailwind_icon,
+    bot_icon ,
+    front_dev_icon,
+    backend_icon,
+    single_page_icon,
+    weeb_icon,
+};
+
+export const workData = [
+    {
+        title: "Salt'n Pepper",
+        description: 'Html Css',
+        bgImage: '/work-2.png',
+        link: 'https://saltn-paper-00.netlify.app/'
+    },
+    {
+        title: 'Clothing website',
+        description: 'Use React',
+        bgImage: '/work-1.png',
+        link: 'https://e-website-00.web.app/'
+    },
+    {
+        title: 'LMS School',
+        description: 'React Firebase Js',
+        bgImage: '/work-3.png',
+        link: 'https://silly-bienenstitch-52aa2d.netlify.app/'
+    },
+    {
+        title: 'Todo App',
+        description: 'React Firebase Js',
+        bgImage: '/work-4.png',
+        link: 'https://clever-profiterole-61bb0c.netlify.app/'
+    },
+]
+
+export const serviceData = [
+    { icon: assets.front_dev_icon, title: 'Frontend Development', description: 'Design responsive and interactive user interfaces using React.js...', link: '' },
+    { icon: assets.single_page_icon, title: 'Single-Page Applications (SPAs)', description: 'Build dynamic SPAs with smooth navigation and fast load times...', link: '' },
+    { icon: assets.weeb_icon, title: 'Custom Web Applications', description: 'Develop tailor-made web apps to meet specific business needs...', link: '' },
+    { icon: assets.backend_icon, title: 'Backend Development', description: 'Develop robust backend systems using Node.js and Express.js...', link: '' },
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript, React Js, Next Js' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: ' Intermediate in Computer Science' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+];
+
+export const toolsData = [
+    assets.vscode, assets.firebase, assets.mongodb, assets.react_icon, assets.git, assets.mui_icon, assets.tailwind_icon, assets.bot_icon 
+];
